27ab99e94b5a8e5909e294764f0949d93287b498
39sanyo <santy8a2003@gmail.com>
2025-08-10 16:05:51 -0400

    added login manager and dashboard

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sr_online.db'
 app.config['SECRET_KEY'] = 'secret'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,8 +29,8 @@ class User(db.Model, UserMixin):
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw=({"placeholder": "username"}))
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw=({"placeholder": "password"}))
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Register")
 
     def validate_username(self, username):
@@ -29,8 +39,8 @@ class RegisterForm(FlaskForm):
             raise ValidationError("That username already exists. Please choose another one.")
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw=({"placeholder": "username"}))
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw=({"placeholder": "password"}))
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Login")
 
 
@@ -53,7 +63,26 @@ def noscam():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
     return render_template("login.html", form = form)
+
+@app.route('/dashboard', methods = ['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+#logout
+@app.route('/logout', methods=('GET', 'POST'))
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 # signup
 @app.route('/signup', methods = ['GET', 'POST'])
@@ -63,11 +92,15 @@ def signup():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
+
         db.session.add(new_user)
         db.session.commit()
+
         return redirect(url_for('login'))
 
     return render_template("signup.html", form = form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
