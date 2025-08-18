d1db52bced1a7f8eff9e481d6b0ef568e8e99e9c
39sanyo <santy8a2003@gmail.com>
2025-08-18 17:59:29 -0400

    Beginnings of character creation, still want to add more deatures

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
+from datetime import datetime
+import sqlite3
 
 # please create config file please
 from config import SECRET_KEY
@@ -29,11 +31,33 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique = True)
     password = db.Column(db.String(80), nullable=False)
+    mc = db.Column(db.String(80), nullable =False, unique = True)
+
+class UserCharacter(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    character_id = db.Column(db.Integer, primary_key = True, unique = True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable = False)
+    character_number = db.Column(db.Integer)
+    name = db.Column(db.String(40), nullable=False)
+    race = db.Column(db.String(20), nullable=False)
+    hero_class = db.Column(db.String(20), nullable = False)
+    background = db.Column(db.String(80), nullable = False)
+    alignment = db.Column(db.String(20), nullable = False)
+    proficiencies = db.Column(db.String(80), nullable = False)
+    equipment = db.Column(db.String(220), nullable = False)
+    backstory = db.Column(db.String(220), nullable = True)
+
+
+    __table_args__ = (
+            db.UniqueConstraint('user_id', 'character_number', name='uq_user_character_number'),
+        )
+
 
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    mc = StringField(validators= [InputRequired(),Length(min=4, max=40)], render_kw={"placeholder": "Minecraft Name"})
     submit = SubmitField("Register")
 
 class LoginForm(FlaskForm):
@@ -41,6 +65,16 @@ class LoginForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Login")
 
+class CharacterCreator(FlaskForm):
+    name = StringField(validators=[InputRequired(), Length(min=1, max=40)], render_kw={"placeholder": "Name"})
+    race = StringField(validators=[InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Race"})
+    hero_class = StringField(validators=[InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Class"})
+    background = StringField(validators=[InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Background"})
+    alignment = StringField(validators=[InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Alignment"})
+    proficiencies = StringField(validators=[InputRequired(), Length(min=1, max=80)], render_kw={"placeholder": "Proficiencies"})
+    equipment = StringField(validators=[InputRequired(), Length(min=1, max=220)], render_kw={"placeholder": "Equipment"})
+    backstory = StringField(validators=[Length(min=1, max=220)], render_kw={"placeholder": "Backstory"})
+    submit = SubmitField("Create Character")
 
 # landing
 @app.route('/', methods = ['GET', 'POST'])
@@ -77,16 +111,39 @@ def login():
 @app.route('/dashboard', methods = ['GET', 'POST'])
 @login_required
 def dashboard():
-    if current_user.is_authenticated:
-        username = current_user.username
-        return render_template('dashboard.html', username = username)
-    else:
-        return "User is not authenticated"
+    username = current_user.username
+    mc = current_user.mc
+    return render_template('dashboard.html', username = username, mc = mc)
 
 @app.route('/dashboard/character', methods = ['GET', 'POST'])
 @login_required
 def character():
-    return render_template('character.html')
+    form = CharacterCreator()
+    username = current_user.username
+    id = current_user.id
+    mc = current_user.mc
+    character = UserCharacter.query.filter_by(user_id=id)
+    
+    if character:
+        # fetching user_character from Databse
+        conn = sqlite3.connect('instance/sr_online.db')
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT name, race, hero_class, background, alignment, proficiencies, equipment, backstory FROM user_character WHERE user_id = {id}")
+        data = cursor.fetchall()
+        conn.close()
+    else:
+        data = "False"
+
+
+    if form.validate_on_submit():
+        new_character = UserCharacter(user_id = id, name=form.name.data, race=form.race.data, hero_class=form.hero_class.data, background=form.background.data, alignment=form.alignment.data, proficiencies=form.proficiencies.data, equipment=form.equipment.data, backstory=form.backstory.data,)
+        db.session.add(new_character)
+        db.session.commit()
+        return redirect(url_for('character'))
+
+
+    return render_template('character.html', username = username, mc=mc, form=form, data = data)
+
 #logout
 @app.route('/logout', methods=('GET', 'POST'))
 @login_required
@@ -104,7 +161,7 @@ def signup():
             return redirect(url_for("signup_fail"))
         else:
             hashed_password = bcrypt.generate_password_hash(form.password.data)
-            new_user = User(username=form.username.data, password=hashed_password)
+            new_user = User(username=form.username.data, password=hashed_password, mc=form.mc.data)
 
             db.session.add(new_user)
             db.session.commit()
